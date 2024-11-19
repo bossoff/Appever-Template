c52e0bfedd2c09ec0fe96a031aacfeb29404179b
Bossoff <rajiatlive@gmail.com>
2024-11-20 00:58:06 +0100

    Modifications

--- src/components/about-two.tsx
+++ src/components/about-two.tsx
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom'
 import about from '../assets/images/phone/half-2.png'
 
 export default function AboutTwo() {
-  return (
+    return (
         <div className="grid md:grid-cols-12 grid-cols-1 items-center gap-6">
             <div className="lg:col-span-5 md:col-span-6 md:order-2 order-1">
                 <div className="pt-6 px-6 rounded-2xl bg-red-500/5 dark:bg-red-500/10 shadow shadow-red-500/20">
-                    <img src={about} alt=""/>
+                    <img src={about} alt="" />
                 </div>
             </div>
-            
+
 
             <div className="lg:col-span-7 md:col-span-6 md:order-1 order-2">
                 <div className="lg:me-10">
                     <h6 className="text-red-500 uppercase text-sm font-bold tracking-wider mb-3">Elegant Design</h6>
-                    <h4 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-bold">Share your photos with <br/> friends easily</h4>
+                    <h4 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-bold">Share your photos with <br /> friends easily</h4>
                     <p className="text-slate-400 max-w-xl">Unleash the power of our platform with a multitude of powerful features, empowering you to achieve your goals.</p>
 
                     <ul className="list-none text-slate-400 mt-6">
@@ -31,5 +31,7 @@ export default function AboutTwo() {
                 </div>
             </div>
         </div>
-  )
+
+
+    )
 }
